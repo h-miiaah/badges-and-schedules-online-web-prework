0cddf0dabbd9e3656dce3e00515d7f542ee58165
Haim D <haim11293@aol.com>
2019-06-27 23:57:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,3 +4,12 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  badge_messages = []
+  attendees.each do |attendee|
+    message = badge_maker(attendee)
+    badge_messages.push(message)
+  end
+  badge_messages
+end
