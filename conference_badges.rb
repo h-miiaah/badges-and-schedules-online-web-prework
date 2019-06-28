9413129489bf8a332f3dc4f671a71f50ceddfa94
Haim D <haim11293@aol.com>
2019-06-28 00:09:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,3 +21,12 @@ def assign_rooms(speakers)
   }
   return assigned_room
 end
+
+def printer (attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |room|
+    puts room
+end
